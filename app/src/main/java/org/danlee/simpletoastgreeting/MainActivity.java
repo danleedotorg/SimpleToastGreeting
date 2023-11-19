f2c692c604d898382c2209b7c85b3a6f0e9306ae
Daniel Lee <danlee1996@proton.me>
2023-11-18 17:25:24 -0700

    made simple app that triggers toast message

--- app/src/main/java/org/danlee/simpletoastgreeting/MainActivity.java
+++ app/src/main/java/org/danlee/simpletoastgreeting/MainActivity.java
@@ -3,12 +3,34 @@ package org.danlee.simpletoastgreeting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    TextView title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        title = findViewById(R.id.title);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = editText.getText().toString();
+
+                Toast.makeText(MainActivity.this, "Welcome " + input + " to our app", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
